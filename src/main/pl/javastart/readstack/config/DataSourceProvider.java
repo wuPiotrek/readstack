43d9e6860c9610ce82e5fbbaf6f366fa892718ef
wuPiotrek <wupiotrek@gmail.com>
2025-01-06 22:26:43 +0100

    added data source provider

---
+++ src/main/pl/javastart/readstack/config/DataSourceProvider.java
@@ -0,0 +1,21 @@
+package pl.javastart.readstack.config;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DataSourceProvider {
+    private static DataSource dataSource;
+
+    private DataSourceProvider() { }
+
+    public static DataSource getDataSource() throws NamingException {
+        if (dataSource == null) {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env/");
+            dataSource = (DataSource) envContext.lookup("jdbc/readstack");
+        }
+        return dataSource;
+    }
+}
